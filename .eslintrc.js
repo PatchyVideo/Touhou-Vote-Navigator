d0616feeb9ed6460632eb5b18e18fc6a2d0c14f3
QiroNT <i@ntz.im>
2022-03-05 21:58:26 +0800

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,88 @@
+// @ts-check
+
+/**
+ * ESLint Configuration File
+ *
+ * Docs: https://eslint.org/docs/user-guide/configuring
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  /**
+   * Config Root
+   */
+  root: true,
+  /**
+   * Custom Rules
+   */
+  rules: {},
+  /**
+   * Custom Groups
+   */
+  overrides: [
+    {
+      files: ['./*.config.js', './*rc.js', '**/scripts/**/*.js', './public/**/*.js'],
+      env: {
+        es2020: true,
+        node: true,
+      },
+      extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    },
+    {
+      files: ['*.js', '*.jsx'],
+      env: {
+        es2020: true,
+        browser: true,
+      },
+      extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      env: {
+        es2020: true,
+        browser: true,
+      },
+      parser: '@typescript-eslint/parser',
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { prefer: 'type-imports', disallowTypeAnnotations: false },
+        ],
+      },
+    },
+    {
+      files: ['*.vue'],
+      env: {
+        es2020: true,
+        browser: true,
+      },
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { prefer: 'type-imports', disallowTypeAnnotations: false },
+        ],
+        'no-undef': 'off',
+      },
+      globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+        withDefaults: 'readonly',
+      },
+    },
+  ],
+}
